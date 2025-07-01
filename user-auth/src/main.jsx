116111d685b3c205ec5c69cc22d4688704a4277b
Santosh Singh Pawar <textingsantosh@gmail.com>
2025-07-01 14:35:12 +0530

    project made

--- user-auth/src/main.jsx
+++ user-auth/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { DataContext, UserContext } from "./Context/UserContext.jsx";
+import { DataContext } from "./Context/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <DataContext>
